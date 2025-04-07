2e991e3c5346acc68fb0ae47aedb500cb3c98e07
zeethonSE <zeethon0@gmail.com>
2025-04-07 22:57:19 +0700

    Updated dist/index.html after deployment

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/the-cocktail-db/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // Ensures @ works
